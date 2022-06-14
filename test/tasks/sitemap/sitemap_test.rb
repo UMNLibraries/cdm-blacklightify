2dcd349708f6f5f622c8550ae1ba4f8296c79b36
ander kierig <akierig@umn.edu>
2022-06-14 14:32:34 -0500

    integrated sitemap into rails test suite
    Previously sitemap_generator called rake task (sitemap:test) instead of being directly integrated directly into the rails test suite. this commit moves the test directly into the test suite and
    
    - removed sitemap:test rake task
    - set config/sitemap.rb to place the generated file in /tmp when running tests
    - deleted the rake task

--- test/tasks/sitemap/sitemap_test.rb
+++ test/tasks/sitemap/sitemap_test.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
-require 'rake'
 require 'test_helper'
+require 'rake'
+require 'sitemap_generator'
 
 # integrate sitemap test into Rails test suite
-class SitemapTaskTest < ActiveSupport::TestCase
-  describe 'rake sitemap:test' do
-    it ':test - will test sitemap generation' do
-      Rake::Task['sitemap:test'].invoke
-      Rake::Task['sitemap:test_clean'].invoke
+class SitemapGeneratorTest < ActiveSupport::TestCase
+  describe 'rake sitemap:create' do
+    def setup
+      Umedia::Application.load_tasks if Rake::Task.tasks.empty?
+    end
+    it ':create - tests sitemap generation' do
+      Rake::Task['sitemap:create'].invoke
+      # test that it has data from solr
+      has_data = "File.size?('tmp/sitemap-test.xml.gz') > 10_000"
+      assert(has_data)
+      Minitest.after_run { File.delete('tmp/sitemap-test.xml.gz') }
     end
   end
 end
