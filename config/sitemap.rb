3fe2109490750b4ae4e42a0f2cc479ae40557c45
Michael Berkowski <mjb@umn.edu>
2022-07-13 07:33:27 -0500

    test varname

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -16,7 +16,7 @@ SitemapGenerator::Interpreter.send :include, Rails.application.routes.url_helper
 SitemapGenerator::Interpreter.send :include, Spotlight::Engine.routes.url_helpers
 
 # create the sitemap itself
-sitemap_test = if Rails.env.test?
+sitemap_opts = if Rails.env.test?
                  {
                    public_path: 'tmp/',
                    filename: 'sitemap-test'
@@ -24,7 +24,7 @@ sitemap_test = if Rails.env.test?
                else
                  {}
                end
-SitemapGenerator::Sitemap.create(sitemap_test) do
+SitemapGenerator::Sitemap.create(sitemap_opts) do
   response['response']['docs'].each do |doc|
     add "/item/#{doc['id']}", changefreq: 'weekly', lastmod: doc['dmmodified_ssi']
   end
