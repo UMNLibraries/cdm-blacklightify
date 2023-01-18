af3f20d2c6fcdf9bedab115d91bfe5d9b507c3ee
Michael Berkowski <mjb@umn.edu>
2023-01-18 15:41:25 -0600

    Load universal viewer from local files

--- prep_uv.sh
+++ prep_uv.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# JS
-cp -R node_modules/universalviewer/dist/umd public/uv
-
-# CSS
-cp -R node_modules/universalviewer/dist/uv.css public/uv/uv.css
+# JS & CSS
+cp -R node_modules/universalviewer/dist/umd public/uv \
+  && cp -R node_modules/universalviewer/dist/uv.css public/uv/uv.css
 
 # Config
-cp -R node_modules/universalviewer/dist/uv-config.json public/uv/config.json
+cp --no-clobber config/uv-config.json.example config/uv-config.json
+ln --symbolic --force config/uv-config.json public/uv/uv-config.json
