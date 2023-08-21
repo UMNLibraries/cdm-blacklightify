14f928258239f6d67f08b25d662bb1124a3d9b08
Scott Lawan <lawanscott@gmail.com>
2023-08-21 15:42:50 -0500

    Define config methods

--- app/presenters/show_presenter.rb
+++ app/presenters/show_presenter.rb
@@ -4,10 +4,35 @@ class ShowPresenter < Blacklight::ShowPresenter
 
   def test_field
     arr = [ "type_ssi", "format_ssim", "sp_physical_format_ssi" ]
-
+    # arr = [ "country_ssi", "sp_country_ssi", "continent_ssi", "sp_continent_ssi" ]
+    # does the document have anything in the array / check if array contains any or all ? . .
+    # document['country_ssi'].present? # just returns true or false
+    # arr.include?('continent_ssi')
+    # document.include?(document['continent_ssi']) # this does not work. can't access document like an array
     arr.each do |field|
-      return document[field].present?
+      return document[field].present? # returns boolean if fields in the document are present in the arr array
     end
+  end
+
+  def test_arr
+    arr = configuration.show_fields.to_a
+    new_arr =[]
+    arr.each do |item| 
+      new_arr.push(item[0])
+    end
+    new_arr
+    # new_arr.each do |field|
+    #   return document[field].present? # returns boolean if fields in the document are present in the arr array
+    # end
+  end
+
+  def type_arr
+    arr = configuration.show_fields.to_a
+    new_arr =[]
+    arr.each do |item| 
+      new_arr.push(item[1][:type])
+    end
+    new_arr
   end
   
   def each_primary_field
