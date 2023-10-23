66aca882b6bf39527bf794af1aa1975f40207b14
Michael Berkowski <mjb@umn.edu>
2023-10-23 12:11:44 -0500

    New routes:
    /item (no id) goes to blank search query, all fields, all records
    /item/:id.json, /catalog/:id.json provides raw json from rawcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,25 @@ Rails.application.routes.draw do
   mount Riiif::Engine => '/images', as: 'riiif'
 
   root to: 'spotlight/exhibits#index'
-  # root to: 'catalog#index' # replaced by spotlight root path
 
   mount Spotlight::Engine, at: 'spotlight'
   mount Blacklight::Engine => '/'
   #  root to: "catalog#index" # replaced by spotlight root path
   concern :searchable, Blacklight::Routes::Searchable.new
 
+  # For compatibility catalog show pages aliased as /item/:id
+  get '/item/:id', to: 'catalog#show'
+
+  # Compatibility with old UMedia JSON views, override what Rails/Blacklight want to do with JSON
+  get '/catalog/:id.json', controller: 'catalog', action: 'raw'
+  get '/item/:id.json', controller: 'catalog', action: 'raw'
+  get '/item', to: redirect('/catalog?search_field=all_fields&q=')
+
+  # Finally allow Blacklight to do its normal stuff
   resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
     concerns :range_searchable
   end
-
-  # For compatibility catalog show pages aliased as /item/:id
-  get '/item/:id', to: 'catalog#show'
 
   devise_for :users
   concern :exportable, Blacklight::Routes::Exportable.new
