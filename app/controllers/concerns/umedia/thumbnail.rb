ad36c21e336a34e911d88098e113a455897cf78b
Michael Berkowski <mjb@umn.edu>
2024-03-05 12:57:14 -0600

    Namespace Thumbnail beneath Umedia

--- app/controllers/concerns/umedia/thumbnail.rb
+++ app/controllers/concerns/umedia/thumbnail.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 # Thumbnail
-module Thumbnail
-  extend ActiveSupport::Concern
-  included do
-    helper_method :thumbnail if respond_to?(:helper_method)
-  end
-
-  def thumbnail(document, _options = {})
-    if document.sidecar.image.attached?
-      thumbnail = document.sidecar.image_url
-    else
-      Umedia::StoreImageJob.perform_later(document.id)
-      thumbnail = document.cdm_thumbnail
+module Umedia
+  module Thumbnail
+    extend ActiveSupport::Concern
+    included do
+      helper_method :thumbnail if respond_to?(:helper_method)
     end
 
-    view_context.render_thumbnail(thumbnail)
+    def thumbnail(document, _options = {})
+      if document.sidecar.image.attached?
+        thumbnail = document.sidecar.image_url
+      else
+        Umedia::StoreImageJob.perform_later(document.id)
+        thumbnail = document.cdm_thumbnail
+      end
+
+      view_context.render_thumbnail(thumbnail)
+    end
   end
 end
