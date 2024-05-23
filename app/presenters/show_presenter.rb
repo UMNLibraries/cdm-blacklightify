1948a46825e31b6f4716e320fb8d228fceb399d5
Michael Berkowski <mjb@umn.edu>
2024-05-23 11:03:08 -0500

    logic error prevented field section headings display

--- app/presenters/show_presenter.rb
+++ app/presenters/show_presenter.rb
@@ -9,18 +9,18 @@ class ShowPresenter < Blacklight::ShowPresenter
     h
   end
 
-  def type_arr(type)    # returns boolean if any field in type_arr is present in the document
-    type_arr =[]
-
+  def type_arr(type)    # returns boolean if any field in field_type_arr is present in the document
+    field_type_arr =[]
     configuration.show_fields.to_a.each do |item|
-      if item[1][:type]==type   # sets the field type
-        type_arr.push(item[0])
+      if item[1][:type] == type   # sets the field type
+        field_type_arr.push(item[0])
       end
     end
 
-    type_arr.each do |field|   # checks document for field presence
-      return document[field].present?
+    field_type_arr.each do |field|   # checks document for field presence
+      return true if document[field].present?
     end
+    return false
   end
 
   def each_field(type)
