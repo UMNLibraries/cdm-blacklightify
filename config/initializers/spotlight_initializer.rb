f6a52822a0f0d351f5c80a3d9cb386c082456881
Scott Lawan <lawanscott@gmail.com>
2024-07-16 14:15:14 -0500

    Update spotlight_initializer thumbnail_field

--- config/initializers/spotlight_initializer.rb
+++ config/initializers/spotlight_initializer.rb
@@ -35,7 +35,7 @@ Spotlight::Engine.config.default_autocomplete_params = { qf: 'id^1000 title_tesi
 # Spotlight::Engine.config.solr_fields.text_suffix = '_tesim'.freeze
 # Spotlight::Engine.config.resource_global_id_field = :"#{config.solr_fields.prefix}spotlight_resource_id#{config.solr_fields.string_suffix}"
 # Spotlight::Engine.config.full_image_field = :full_image_url_ssm
-# Spotlight::Engine.config.thumbnail_field = :thumbnail_url_ssm
+Spotlight::Engine.config.thumbnail_field = :object_ssi
 
 # ==> Uploaded item configuration
 # Spotlight::Engine.config.upload_fields = [
