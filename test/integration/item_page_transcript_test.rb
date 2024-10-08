2dc606f6d4e0af036c217737e505a1f01f213688
Scott Lawan <lawanscott@gmail.com>
2024-10-08 14:33:00 -0500

    WIP: Create item page transcript test

---
+++ test/integration/item_page_transcript_test.rb
@@ -0,0 +1,23 @@
+# require 'test_helper'
+
+class ItemPageTranscriptTest < ActionDispatch::IntegrationTest
+  test 'transcript display' do
+    get root_path + '/catalog' + '/p16022coll613:15'
+    assert_response :success
+    assert_template partial: '_transcript'
+
+    assert_select 'div.modal-body' do
+      assert_select 'div#transcript_hello', count: 1
+    end
+  end
+
+  test 'transcript display if transcript_test' do
+    get root_path + '/catalog' + '/p16022coll171:3716'
+    assert_response :success
+    assert_template partial: '_transcript'
+
+    assert_select 'div.modal-body' do
+      assert_select 'div.modal-header', text: /.+/, count: 1
+    end
+  end
+end
