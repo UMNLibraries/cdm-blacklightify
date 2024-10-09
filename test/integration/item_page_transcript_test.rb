ab22006bf0ffb66833b1aa143ddda58cbe889f89
Scott Lawan <lawanscott@gmail.com>
2024-10-09 12:55:17 -0500

    WIP: Test transcript#index

--- test/integration/item_page_transcript_test.rb
+++ test/integration/item_page_transcript_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class ItemPageTranscriptTest < ActionDispatch::IntegrationTest
+  test 'test transcript#index' do
+    get root_path + '/catalog' + '/p16022coll613:15' + '/transcript'
+    assert_response :success
+    assert_select 'div.modal-body'
+  end
+
   test 'transcript display' do
     get root_path + '/catalog' + '/p16022coll613:15'
     assert_response :success
     assert_template partial: '_transcript'
-    
     assert_select 'div.modal-body' do
       assert_select 'div#transcript_hello'
     end
@@ -15,7 +20,6 @@ class ItemPageTranscriptTest < ActionDispatch::IntegrationTest
     get root_path + '/catalog' + '/p16022coll171:3716'
     assert_response :success
     assert_template partial: '_transcript'
-
     assert_select 'div.modal-body' do
       assert_select 'div.modal-header', text: /.+/, count: 1
     end
