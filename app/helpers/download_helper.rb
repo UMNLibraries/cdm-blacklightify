a16f16c625200533296c25136210c3b89965ba7c
Scott Lawan <lawanscott@gmail.com>
2025-01-31 12:07:59 -0600

    WIP: Handle images in SERP download link

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -2,7 +2,10 @@ module DownloadHelper
   include Umedia
   
   def download_path
+    @document[:viewer_type] == "COMPOUND_PARENT_NO_VIEWER" ? 
     # compound objects
-    "https://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{@document[:id].split(':')[0]}/id/#{@document[:id].split(':')[1]}/filename/print/page/download/fparams/forcedownload"
+    "https://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{@document[:id].split(':')[0]}/id/#{@document[:id].split(':')[1]}/filename/print/page/download/fparams/forcedownload" :
+    # image
+    "https://cdm16022.contentdm.oclc.org/utils/getstream/collection/#{@document[:id].split(':')[0]}/id/#{@document[:id].split(':')[1]}"
   end
 end
