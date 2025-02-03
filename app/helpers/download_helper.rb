5674ae4123d630c90f40049abc376e9bac101752
Scott Lawan <lawanscott@gmail.com>
2025-02-03 11:51:18 -0600

    WIP: Add download link for kaltura assets

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -8,4 +8,12 @@ module DownloadHelper
     # image
     "https://cdm16022.contentdm.oclc.org/utils/getstream/collection/#{@document[:id].split(':')[0]}/id/#{@document[:id].split(':')[1]}"
   end
+
+  def type_selector
+    @document[:kaltura_audio] ? 
+      # audio
+      'https://cdnapisec.kaltura.com/p/1369852/sp/136985200/playManifest/entryId/' + @document[:kaltura_audio] + '/flavorId/1_atuqqpf6/format/url/protocol/http/a.mp4' : 
+      # video
+      'https://cdnapisec.kaltura.com/p/1369852/sp/136985200/playManifest/entryId/' + @document[:kaltura_video] + '/flavorId/1_uivmmxof/format/url/protocol/http/a.mp4'
+  end
 end
