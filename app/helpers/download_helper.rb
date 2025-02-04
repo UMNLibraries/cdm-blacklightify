1cea23d55f5c44b1fac636d7173bf9f68c2e284a
Scott Lawan <lawanscott@gmail.com>
2025-02-04 14:41:25 -0600

    WIP: testing playlist "download"

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -16,4 +16,20 @@ module DownloadHelper
       # video
       'https://cdnapisec.kaltura.com/p/1369852/sp/136985200/playManifest/entryId/' + @document[:kaltura_video] + '/flavorId/1_uivmmxof/format/url/protocol/http/a.mp4'
   end
+
+  def playlist
+  # playlist ? it is an older embed frame ? @document[:kaltura_audio_playlist]
+  # if unable to execute on playlist download, will need logic to not present link for such items . . . can just look for kaltura_audio_playlist field . . .
+  # 'https://cdnapi.kaltura.com/html5/html5lib/v2.50/mwEmbedFrame.php/p/2311101/uiconf_id/41902002/entry_id/1_89r40of6?wid=_1369852'
+
+    # check if kaltura_audio_playlist field is present
+    @document[:kaltura_audio_playlist] != nil
+
+  end
+
+  def single_embed
+    if playlist == true
+      'https://cdnapi.kaltura.com/html5/html5lib/v2.50/mwEmbedFrame.php/p/2311101/uiconf_id/41902002/entry_id/' + @document[:kaltura_audio_playlist] + '?wid=_1369852'
+    end
+  end
 end
