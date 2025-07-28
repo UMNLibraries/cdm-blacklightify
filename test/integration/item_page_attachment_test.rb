1d719d56b51645a292c072ecdc8131ac445ac85f
Scott Lawan <lawanscott@gmail.com>
2025-07-28 13:26:57 -0500

    Add test, attachment tab for child of parent

--- test/integration/item_page_attachment_test.rb
+++ test/integration/item_page_attachment_test.rb
@@ -3,14 +3,12 @@ require 'test_helper'
 class ItemPageAttachmentTest < ActionDispatch::IntegrationTest
   test 'test transcript#index' do
     get root_path + '/catalog' + '/p16022coll613:15' + '/transcript'
-
     assert_response :success
     assert_select 'div.modal-body'
   end
 
-  test 'transcript display' do
+  test 'transcript display, compound' do
     get root_path + '/catalog' + '/p16022coll613:15'
-
     assert_response :success
     assert_template partial: '_transcript'
     assert_select 'ul.nav' do
@@ -18,6 +16,15 @@ class ItemPageAttachmentTest < ActionDispatch::IntegrationTest
     end
     assert_select 'div.tab-content' do
       assert_select 'div#transcript_content'
+    end
+  end
+
+  test 'transcript display, child of parent' do
+    get root_path + '/catalog' + '/p16022coll265:23673'
+    assert_response :success
+    assert_select 'div.p-3' do
+      assert_select 'h3'
+      assert_select 'p'
     end
   end
 
@@ -34,7 +41,6 @@ class ItemPageAttachmentTest < ActionDispatch::IntegrationTest
 
   test 'transcript tab !display (transcript_tab helper)' do
     get root_path + '/catalog' + '/p16022coll208:5292'
-
     assert_response :success
     assert_select 'ul.nav' do
       assert_select 'li', 1
@@ -43,13 +49,21 @@ class ItemPageAttachmentTest < ActionDispatch::IntegrationTest
 
   test 'attachment tab display a/v item with image attachment' do
     get root_path + '/catalog' + '/p16022coll215:4'
-  
+    assert_response :success
+    assert_select 'div.tab-content' do
+      # iiif viewer
+      assert_select 'div#uv'
+    end
   end
 
   test 'attachment tab display a/v item with pdf attachment' do
     get root_path + '/catalog' + '/p16022coll171:3699'
-
-    # assert on loading spinner . . .
-
+    assert_response :success
+    assert_select 'div.tab-content' do
+      # embed tag
+      assert_select 'embed'
+      # loading spinner . . .
+      assert_select 'div.rounded-circle'
+    end
   end
 end
