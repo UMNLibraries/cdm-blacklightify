2907dc6d0f4e9db30460485c5bd7a0d959762570
Scott Lawan <lawanscott@gmail.com>
2025-07-29 14:05:17 -0500

    Resolve subject_fast faceting issue

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -102,8 +102,9 @@ class CatalogController < ApplicationController
     # Format / format_name
     config.add_facet_field 'format_name', label: 'Format', limit: 4, collapse: false
     # Subject / subject
-    # config.add_facet_field 'subject_fast_ss', label: 'Subject', limit: 4, collapse: false
     config.add_facet_field 'subject_ssm', label: 'Subject', limit: 4, collapse: false
+    config.add_facet_field 'subject_fast_ssim', label: 'FAST Subject Headings', limit: 4, collapse: false
+
     # Created / date_created
     config.add_facet_field 'date_created', label: 'Date created', limit: 4, collapse: false
     # Collection / collection_name
@@ -142,7 +143,8 @@ class CatalogController < ApplicationController
     # Subject / subject
     config.add_index_field 'subject_ssm', label: 'Subjects', link_to_facet: true, highlight: true,
                                       separator_options: { two_words_connector: '; ', words_connector: '; ', last_word_connector: '; ' }
-
+    config.add_index_field 'subject_fast_ssim', label: 'FAST Subject Headings', link_to_facet: true, highlight: true,
+                                      separator_options: { two_words_connector: '; ', words_connector: '; ', last_word_connector: '; ' }
     # Thumbnails - A helper method that looks for attached image from solr_document_sidecar
     config.index.thumbnail_method = :thumbnail
 
