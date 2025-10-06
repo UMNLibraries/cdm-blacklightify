92e281b707e4554092f89bdf48ec070fa1bd985e
Scott Lawan <lawanscott@gmail.com>
2025-10-06 15:15:43 -0500

    Add error handling

--- app/services/oclc_subject_fast_service.rb
+++ app/services/oclc_subject_fast_service.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'open-uri'
 
 class OclcSubjectFastService
   def initialize(id)
@@ -6,8 +7,23 @@ class OclcSubjectFastService
   end
 
   def fast_data
-    url = @id + '.rdf.xml'
-    doc = Nokogiri::XML(URI.open(url))
-    term = doc.xpath("//skos:prefLabel").collect(&:text)[0]
-  end  
+    begin
+      url = @id + '.rdf.xml'
+      doc = Nokogiri::XML(URI.open(url))
+      term = doc.xpath("//skos:prefLabel").collect(&:text)[0]
+    # if uri returns 404 (may not be necessary)
+    rescue OpenURI::HTTPError => e
+      if e.message.include?('404')
+        e.message
+      else
+        raise e
+      end
+    end
+  end
+
+  # if subject_fast field is entered as a term, not the oclc uri (may not be necessary)
+  # leaving these in for dev purposes . . .
+  def uri_check
+    @id.downcase.include?("id.worldcat.org")
+  end
 end
