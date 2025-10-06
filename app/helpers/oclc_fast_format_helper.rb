de6bc168941c4b05bcdbff1d86171632668b1474
Scott Lawan <lawanscott@gmail.com>
2025-10-06 12:42:34 -0500

    Update oclc subject fast helpers

--- app/helpers/oclc_fast_format_helper.rb
+++ app/helpers/oclc_fast_format_helper.rb
@@ -1,11 +1,6 @@
 module OclcFastFormatHelper
   include Umedia
 
-  # get the oclc uri
-  def subject_loop
-    array = @document[:subject_fast]
-  end
-
   def subject_name
     @document[:subject_fast]
   end
