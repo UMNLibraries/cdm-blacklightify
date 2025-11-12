862215f79ee4acfa4c36eb1f85df8e435187a816
Scott Lawan <lawanscott@gmail.com>
2025-11-12 12:42:08 -0600

    Refactoring

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -3,6 +3,6 @@ class SubjectController < ApplicationController
     @url = params[:url]
     @term = OclcSubjectFastService.new(@url).fast_data
 
-    render partial: 'metadata_field_component_sf_two', locals: { url: @url, term: @term }
+    render partial: 'metadata_field_component_sf_term', locals: { url: @url, term: @term }
   end
 end
