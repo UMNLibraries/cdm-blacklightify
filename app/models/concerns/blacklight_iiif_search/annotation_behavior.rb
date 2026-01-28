0e4e3e496aa3976afcb8a539f2b6576e6cbd4e6a
Scott Lawan <lawanscott@gmail.com>
2026-01-28 11:14:32 -0600

    Add SearchBehavior and SearchAnnocationBehavior modules

---
+++ app/models/concerns/blacklight_iiif_search/annotation_behavior.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# customizable behavior for IiifSearchAnnotation
+module BlacklightIiifSearch
+  module AnnotationBehavior
+    ##
+    # Create a URL for the annotation
+    # @return [String]
+    def annotation_id
+      # "#{controller.solr_document_url(parent_document[:id])}/canvas/#{document[:id]}/annotation/#{hl_index}"
+      "https://cdm16022.contentdm.oclc.org/iiif/#{document[:id]}/annotation/a#{document[:child_index]}"
+    end
+
+    ##
+    # Create a URL for the canvas that the annotation refers to
+    # @return [String]
+    def canvas_uri_for_annotation
+      # "#{controller.solr_document_url(parent_document[:id])}/canvas/#{document[:id]}" + coordinates
+      "https://cdm16022.contentdm.oclc.org/iiif/#{document[:id]}/canvas/c#{document[:child_index]}" + coordinates
+    end
+
+    ##
+    # return a string like "#xywh=100,100,250,20"
+    # corresponding to coordinates of query term on image
+    # local implementation expected, value returned below is just a placeholder
+    # @return [String]
+    def coordinates
+      return '' unless query
+      '#xywh=0,0,0,0'
+    end
+  end
+end
