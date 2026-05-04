d9f5926c005d429be6229c88d1f97ee7ccbcd305
Michael Berkowski <mjb@umn.edu>
2026-05-04 11:33:36 -0500

    resolve merge conf

--- app/services/getty_json_service.rb
+++ app/services/getty_json_service.rb
@@ -4,15 +4,13 @@ class GettyJsonService
   end
 
   def aat_data
-    url = 'https://vocab.getty.edu/aat/' + @id + '.json'
-    res = Net::HTTP.get_response(URI(url))
-    parsed_response = res.body
-    JSON.parse(parsed_response)
-  end  
+    @res ||= Net::HTTP.get_response(URI( 'https://vocab.getty.edu/aat/' + @id + '.json' )).body
+    JSON.parse(@res) rescue {}
+  end
 
   # get the format id, use that key for caching
   def att_def_id
-    aat_data.dig('id').match('aat\/(.*)')&.captures[0] 
+    aat_data.dig('id').match('aat\/(.*)')&.captures[0] rescue nil
   end
 
   def att_definition
