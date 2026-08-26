95e9a90add221ce5fa983c59065c17c45a00443a
Bill Tantzen <tantz001@umn.edu>
2026-08-26 12:44:02 -0500

    cleanup api for kaltura_helper.

--- app/helpers/kaltura_helper.rb
+++ app/helpers/kaltura_helper.rb
@@ -3,18 +3,29 @@
 ##
 # KalturaHelper
 module KalturaHelper
-
-  settings = Rails.application.config_for(:kaltura)
-
-  BASE_URL = settings[:url]
-  PARTNER_ID = settings[:partner_id].to_s
-  UICONF_ID = settings[:uiconf_id].to_s
-  
   def kaltura_video_url
-    BASE_URL + '/p/' + PARTNER_ID + '/embedPlaykitJs/uiconf_id/' + UICONF_ID
+    "#{kaltura_base_url}/p/#{kaltura_partner_id}/embedPlaykitJs/uiconf_id/#{kaltura_uiconf_id}"
   end
 
   def kaltura_audio_url
     kaltura_video_url
-  end  
+  end
+
+  private
+
+  def kaltura_settings
+    @kaltura_settings ||= Rails.application.config_for(:kaltura).with_indifferent_access
+  end
+
+  def kaltura_base_url
+    ENV.fetch('KALTURA_URL', kaltura_settings[:url]).to_s
+  end
+
+  def kaltura_partner_id
+    ENV.fetch('KALTURA_PARTNER_ID', kaltura_settings[:partner_id]).to_s
+  end
+
+  def kaltura_uiconf_id
+    ENV.fetch('KALTURA_UICONF_ID', kaltura_settings[:uiconf_id]).to_s
+  end
 end
