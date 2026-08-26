3e4fc2484d807893e0200c34cb23a45597e92c34
Bill Tantzen <tantz001@umn.edu>
2026-08-26 14:34:05 -0500

    leverage ENV for kaltura settings.

--- app/helpers/kaltura_helper.rb
+++ app/helpers/kaltura_helper.rb
@@ -5,6 +5,14 @@ require 'uri'
 ##
 # KalturaHelper
 module KalturaHelper
+  def kaltura_single_embed_script_src
+    kaltura_embed_iframe_js_url(uiconf_id: kaltura_player_uiconf_id_single)
+  end
+
+  def kaltura_compound_embed_script_src
+    kaltura_embed_iframe_js_url(uiconf_id: kaltura_player_uiconf_id_compound)
+  end
+
   def kaltura_video_url(entry_id)
     kaltura_embed_url(entry_id: entry_id)
   end
@@ -19,20 +27,28 @@ module KalturaHelper
 
   private
 
-  def kaltura_settings
-    @kaltura_settings ||= Rails.application.config_for(:kaltura).with_indifferent_access
-  end
-
   def kaltura_base_url
-    kaltura_settings[:url].to_s
+    ENV.fetch('KALTURA_URL', 'https://cdnapisec.kaltura.com')
   end
 
   def kaltura_partner_id
-    kaltura_settings[:partner_id].to_s
+    ENV.fetch('KALTURA_PARTNER_ID')
+  end
+
+  def kaltura_sub_partner_id
+    ENV.fetch('KALTURA_SUB_PARTNER_ID', "#{kaltura_partner_id}00")
   end
 
   def kaltura_uiconf_id
-    kaltura_settings[:uiconf_id].to_s
+    ENV['KALTURA_UICONF_ID'] || kaltura_player_uiconf_id_single
+  end
+
+  def kaltura_player_uiconf_id_single
+    ENV.fetch('KALTURA_PLAYER_UICONF_ID_SINGLE')
+  end
+
+  def kaltura_player_uiconf_id_compound
+    ENV.fetch('KALTURA_PLAYER_UICONF_ID_COMPOUND')
   end
 
   def kaltura_embed_url(entry_id: nil, playlist_id: nil)
@@ -41,5 +57,9 @@ module KalturaHelper
     query[:playlist_id] = playlist_id if playlist_id.present?
 
     "#{kaltura_base_url}/p/#{kaltura_partner_id}/embedPlaykitJs/uiconf_id/#{kaltura_uiconf_id}?#{URI.encode_www_form(query)}"
+  end
+
+  def kaltura_embed_iframe_js_url(uiconf_id:)
+    "#{kaltura_base_url}/p/#{kaltura_partner_id}/sp/#{kaltura_sub_partner_id}/embedIframeJs/uiconf_id/#{uiconf_id}/partner_id/#{kaltura_partner_id}"
   end
 end
