cb2fab83d5ae967b97c67d96b7da634249175b37
Michael Berkowski <mjb@umn.edu>
2026-08-27 13:28:43 -0500

    Remove the extra initializer and Concern because ActiveSupport does not want an attr_accessor defined

--- config/initializers/spotlight_exhibit_umedia_extensions.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Adds a set_spec attribute to Spotlight::Exhibit and provides a method to find or create an exhibit by set_spec.
-Rails.application.config.to_prepare do
-  Spotlight::Exhibit.include Umedia::ExhibitExtensions
-end
